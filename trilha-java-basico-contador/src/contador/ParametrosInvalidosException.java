69a9d2ac2938e970f5b250be1a73228b52e365b0
lucher04 <lucasgabrielnavassabino@gmail.com>
2024-10-25 19:23:47 -0300

    Add dio-trilha-java-basico-contador to unified repository

---
+++ trilha-java-basico-contador/src/contador/ParametrosInvalidosException.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package contador;
+
+/**
+ *
+ * @author lucas
+ */
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
+
